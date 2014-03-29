a6f34dd6443fba56df8f45be63ea600c9d066e89
Andrew Egeler <andrew@egeler.us>
2014-03-29 12:15:27 -0400

    Support non-Build.pm installers

--- Build.pm
+++ Build.pm
@@ -4,12 +4,14 @@ use Shell::Command;
 
 class Build is Panda::Builder {
     method build($workdir) {
+        rm_f("$workdir/lib/MIME/Base64.pm6");
         if $*VM<name> eq 'parrot' {
             cp("$workdir/lib/MIME/Base64.pm6.parrot", "$workdir/lib/MIME/Base64.pm6");
+            cp("$workdir/lib/MIME/Base64/PIR.pm6.parrot", "$workdir/lib/MIME/Base64/PIR.pm6");
         } else {
             cp("$workdir/lib/MIME/Base64.pm6.perl", "$workdir/lib/MIME/Base64.pm6");
-            rm_f("$workdir/lib/MIME/Base64/PIR.pm6");
         }
+        rm_f("$workdir/lib/MIME/Base64/PIR.pm6.parrot");
         rm_f("$workdir/lib/MIME/Base64.pm6.parrot");
         rm_f("$workdir/lib/MIME/Base64.pm6.perl");
     }
