888938e1139967f132756acdc98af8b2a2bd0ee0
RS <glassflag@users.noreply.github.com>
2014-05-30 01:24:06 +1200

    $*VM<name> is deprecated

--- Build.pm
+++ Build.pm
@@ -5,7 +5,7 @@ use Shell::Command;
 class Build is Panda::Builder {
     method build($workdir) {
         rm_f("$workdir/lib/MIME/Base64.pm6");
-        if $*VM<name> eq 'parrot' {
+        if $*VM.name eq 'parrot' {
             cp("$workdir/lib/MIME/Base64.pm6.parrot", "$workdir/lib/MIME/Base64.pm6");
             cp("$workdir/lib/MIME/Base64/PIR.pm6.parrot", "$workdir/lib/MIME/Base64/PIR.pm6");
         } else {
